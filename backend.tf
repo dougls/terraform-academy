6a570796c26a0447d6b5a8f187c8b7dc0a80333c
dougls <douglimadasilva@gmail.com>
2025-02-06 19:19:04 -0300

    terraform-cloud

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "nett-backend-tf"
-    key = "fiap/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "nett-backend-tf"
+#     key = "fiap/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
--- vars.tf
+++ vars.tf
@@ -7,7 +7,7 @@ variable "projectName" {
 }
 
 variable "labRole" {
-  default = "arn:aws:iam::759212231558:role/LabRole"
+  default = "arn:aws:iam::239569854352:role/LabRole"
 }
 
 variable "accessConfig" {
@@ -23,7 +23,7 @@ variable "instanceType" {
 }
 
 variable "principalArn" {
-  default = "arn:aws:iam::759212231558:role/voclabs"
+  default = "arn:aws:iam::239569854352:role/voclabs"
 }
 
 variable "policyArn" {
