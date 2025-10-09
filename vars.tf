75d0c0fb13ec81a0765b4b8edcd5ed3e5ba14bb2
Douglas Martins <32877533+dougls@users.noreply.github.com>
2025-10-09 19:24:55 -0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -7,7 +7,7 @@ variable "projectName" {
 }
 
 variable "labRole" {
-  default = "arn:aws:iam::239569854352:role/LabRole"
+  default = "arn:aws:iam::891377101229:role/LabRole"
 }
 
 variable "accessConfig" {
@@ -23,7 +23,7 @@ variable "instanceType" {
 }
 
 variable "principalArn" {
-  default = "arn:aws:iam::239569854352:role/voclabs"
+  default = "arn:aws:iam::891377101229:role/voclabs"
 }
 
 variable "policyArn" {
